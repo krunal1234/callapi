33b517c7b74a67bbad23080ec4ec99752476f178
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2024-11-25 09:33:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,24 @@ app.get('/', (req, res) => {
     res.redirect('/api-docs');
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} with PID: ${process.pid}`);
-});
+
+// Start server in cluster mode
+if (cluster.isPrimary) {
+    const workerPIDs = [];
+    for (let i = 0; i < totalCPUs; i++) {
+        const worker = cluster.fork();
+        workerPIDs.push(worker.process.pid);
+    }
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+    console.log('Worker PIDs:', workerPIDs);
+} else {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} with PID: ${process.pid}`);
+    });
+}
+
+// Export the app for testing or other purposes
+export default app;
