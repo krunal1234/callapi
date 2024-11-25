4d5428deb1c81c1e41bd345ab397f01d29c9929c
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2024-11-25 23:21:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
 const express = require('express');
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const path = require('path');
-const fs = require('fs');
-const getUser = require('./api/getUser.js');
-const chat = require('./api/chat.js');
-const imageAnalyser = require('./api/imageAnalyser.js');
-const FileReader = require('./api/FileReader.js');
-const SpeechToText = require('./api/SpeechToText.js');
-const ImageReader = require('./api/ImageReader.js');
-const TextToSpeech = require('./api/TextToSpeech.js');
-const BackgroundRemover = require('./api/BackgroundRemover.js');
-
-// Serve the swagger.json file statically
-
-// Set up Swagger UI
-const swaggerDocument = require('../swagger.json');
-const { redirect } = require('express/lib/response.js');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-const API_KEY = 'test';
-function verifyApiKey(req, res, next) {
-    const apiKey = req.headers['api-key']; // Look for 'api-key' in the request headers
-
-    if (apiKey && apiKey === API_KEY) {
-        return next(); // If the key matches, allow the request to continue
-    }
-
-    // If the API key is missing or incorrect, respond with an error
-    res.status(403).json({ message: 'Forbidden: Invalid API Key' });
-}
-
-app.use('/api', verifyApiKey);
-
-app.use('/api/getUser', getUser);
-app.use('/api/chat', chat);
-app.use('/api/imageAnalyser', imageAnalyser);
-app.use('/api/FileReader', FileReader);
-app.use('/api/SpeechToText', SpeechToText);
-app.use('/api/ImageReader', ImageReader);
-app.use('/api/TextToSpeech', TextToSpeech);
-app.use('/api/BackgroundRemover', BackgroundRemover);
-
-// Root route (Fixes "Cannot GET /" error)
-app.get('/', (req, res) => {
-    res.redirect('/api-docs');
-});
-
 
 // Start server in cluster mode
 if (cluster.isPrimary) {
