c42976f16915494bba51e73f9cd271c63a5c6c75
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2024-11-26 10:06:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,42 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
+
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const path = require('path');
-const fs = require('fs');
-const getUser = require('./api/getUser.js');
-const chat = require('./api/chat.js');
-const imageAnalyser = require('./api/imageAnalyser.js');
-const FileReader = require('./api/FileReader.js');
-const SpeechToText = require('./api/SpeechToText.js');
-const ImageReader = require('./api/ImageReader.js');
-const TextToSpeech = require('./api/TextToSpeech.js');
-const BackgroundRemover = require('./api/BackgroundRemover.js');
-const cluster = require('cluster');
-const os = require('os');
+const port = process.env.PORT || 3000;
 
-const totalCPUs = os.cpus().length;
-// Serve the swagger.json file statically
-app.use('/swagger.json', express.static(path.join(__dirname, 'swagger.json')));
+app.use(cors());
+app.use(express.json());
 
-// Set up Swagger UI
-const swaggerDocument = require('./public/swagger.json');
-const { redirect } = require('express/lib/response.js');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-const API_KEY = 'test';
-function verifyApiKey(req, res, next) {
-    const apiKey = req.headers['api-key']; // Look for 'api-key' in the request headers
-
-    if (apiKey && apiKey === API_KEY) {
-        return next(); // If the key matches, allow the request to continue
-    }
-
-    // If the API key is missing or incorrect, respond with an error
-    res.status(403).json({ message: 'Forbidden: Invalid API Key' });
+// Swagger Configuração
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API - Flex Test',
+      version: '1.0.0',
+      description: 'Documentação com Swagger'
+    },
+  },
+  apis: ['/src/docs/swagger/**/*.yaml']
 }
 
-app.use('/api', verifyApiKey);
+const swaggerSpec = swaggerJSDoc(options);
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
-app.use('/api/getUser', getUser);
-app.use('/api/chat', chat);
-app.use('/api/imageAnalyser', imageAnalyser);
-app.use('/api/FileReader', FileReader);
-app.use('/api/SpeechToText', SpeechToText);
-app.use('/api/ImageReader', ImageReader);
-app.use('/api/TextToSpeech', TextToSpeech);
-app.use('/api/BackgroundRemover', BackgroundRemover);
+app.use('/api-swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL,
+}));
 
-// Root route (Fixes "Cannot GET /" error)
-app.get('/', (req, res) => {
-    res.redirect('/api-docs');
+// Start server
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
 
-if (cluster.isPrimary) {
-    const workerPIDs = [];
-    for (let i = 0; i < totalCPUs; i++) {
-        const worker = cluster.fork();
-        workerPIDs.push(worker.process.pid);
-    }
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-    });
-    console.log('Worker PIDs:', workerPIDs);
-} else {
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT} with PID: ${process.pid}`);
-    });
-}
 
-// Export the app for testing or other purposes
 export default app;
