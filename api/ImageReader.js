e081c8d49340244e3acc275df13945c90e7b81b3
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2024-11-27 19:49:40 +0530

    Update ImageReader.js

--- api/ImageReader.js
+++ api/ImageReader.js
@@ -1,4 +1,4 @@
-import { ApexImageReader } from 'apexify';
+import { ApexImageReader } from 'apexify.js';
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
